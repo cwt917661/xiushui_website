1c260be7e375945a7c47558a722abead92bc6317
cwt917661 <cwt917661@hotmail.com>
2023-09-21 19:17:53 +0800

    Update create_user.sql

--- docker/init_sql/create_user.sql
+++ docker/init_sql/create_user.sql
@@ -1,2 +1,2 @@
-CREATE USER 'xiushui_website'@'%' IDENTIFIED WITH caching_sha2_password BY 'administrator';
-GRANT ALL PRIVILEGES ON xiushui_website.* TO 'xiushui_website'@'%';
+CREATE USER '{user_name}'@'%' IDENTIFIED WITH caching_sha2_password BY '{password}';
+GRANT ALL PRIVILEGES ON {schema_name}.* TO '{user_name}'@'%';
